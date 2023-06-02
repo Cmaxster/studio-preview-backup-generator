cd8da6dfce57c190c93b4fd7d5c767c01e290ad7
cmaxster <cmaxster@gmail.com>
2023-06-02 09:54:22 -0400

    Adding script to save screenshots to desktop downloads folder.

--- background.js
+++ background.js
@@ -3,10 +3,10 @@ console.log('>> [background] loaded!');
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   console.log('>> [background] chrome runtime message heard!');
   
-  if (request.command === "takeScreenshot") { // t
+  if (request.command === "takeScreenshot") {
     console.log('>> [background] takeScreenshot heard, taking screenshot..')
     // Forward the takeScreenshot command to the content script
-    chrome.tabs.query({active:true, currentWindow: true, highlighted: true}, function(tabs) {
+    chrome.tabs.query({currentWindow: true, highlighted: true}, function(tabs) {
       if (tabs.length > 0) {
         chrome.tabs.sendMessage(tabs[0].id, {command: "takeScreenshot"});
       } else {
@@ -47,6 +47,17 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
             // Capture the screenshot
             chrome.tabs.captureVisibleTab(tab.windowId, {format: "jpeg"}, function(dataUrl) {
               // Handle the screenshot data
+              const blob = dataURLtoBlob(dataUrl);
+              const objectUrl = URL.createObjectURL(blob);
+
+              // Generate a filename for the screenshot
+              const filename = `screenshot_${width}x${height}.jpeg`;
+
+              // Download the screenshot
+              chrome.downloads.download({
+                url: objectUrl,
+                filename: filename
+              });
             });
           });
         });
@@ -56,3 +67,13 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     });
   }
 });
+
+// Function to convert dataURL to Blob object
+function dataURLtoBlob(dataurl) {
+  const arr = dataurl.split(','), mime = arr[0].match(/:(.*?);/)[1];
+  const bstr = atob(arr[1]), n = bstr.length, u8arr = new Uint8Array(n);
+  while(n--){
+    u8arr[n] = bstr.charCodeAt(n);
+  }
+  return new Blob([u8arr], {type:mime});
+}
